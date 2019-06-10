ce5f87d6c707aa02157f556b918eba5c94fd3bce
Carl Pearson <pearson@illinois.edu>
2019-06-10 15:52:45 -0500

    dynamic algorithm selection benchmarking script

---
+++ scripts/run-dyn.sh
@@ -0,0 +1,19 @@
+#! env bash
+
+set -eou pipefail
+
+GRAPH_DIR=$1
+EXPERIMENT_DIR=`date +%Y%m%d-%H%M%S`_dyn
+
+echo $EXPERIMENT_DIR
+mkdir -p $EXPERIMENT_DIR
+
+src/pangolin-version >> $EXPERIMENT_DIR/version.txt
+
+for g in $GRAPH_DIR/*.bel; do
+    for bs in 32 64 128 256 512; do
+        echo $bs $g
+        echo -ne $bs,'\t' >> $EXPERIMENT_DIR/run.csv
+        src/benchmark-dyn -g 0 --prefetch-async --debug -n 5 $g >> $EXPERIMENT_DIR/run.csv 2>>$EXPERIMENT_DIR/run.log
+    done
+done
