80d95e13dc9ed75cf369947f44faffaa2285166a
Mohammad Almasri <almasri3@hal-login.hal.ncsa.illinois.edu>
2019-06-11 10:32:36 -0500

    multi node by k

--- src/benchmark_multiGPU_binary_ktruss.cu
+++ src/benchmark_multiGPU_binary_ktruss.cu
@@ -1,3 +1,5 @@
+#include <mpi.h>
+
 #include <fmt/format.h>
 #include <iostream>
 
@@ -41,6 +43,22 @@ int getMaxK(std::map<UT, int> degree)
 int main(int argc, char **argv) {
 
 
+  MPI_Init(&argc, &argv);
+
+  // get the number of ranks
+  int worldSize;
+  MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+  MPI_Comm_set_errhandler(MPI_COMM_WORLD,MPI_ERRORS_RETURN);
+
+  // Get the rank of the process
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (0 == rank) {
+    printf("detected world size of %d\n", worldSize);
+  }
+
+
+  
 
   pangolin::init(); 
   pangolin::Config config;
@@ -133,88 +151,93 @@ int main(int argc, char **argv) {
   std::vector<double> times;
   uint64_t nnz;
   uint64_t tris;
-  for (int i = 0; i < iters; ++i) {
-    // create csr
-    start = std::chrono::system_clock::now();
-    auto upperTriangular = [](pangolin::EdgeTy<UT> e) {
-      return true; //e.first < e.second;
-    };
-    auto csr = pangolin::COO<UT>::from_edges(edges.begin(), edges.end(),
-                                                   upperTriangular);
-    LOG(debug, "nnz = {}", csr.nnz());
-    elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
-    LOG(info, "create CSR time {}s", elapsed);
 
-    // read-mostly
-    nvtxRangePush("read-mostly");
-    start = std::chrono::system_clock::now();
-    if (readMostly) {
-      for (const auto &gpu : gpus) {
-        csr.read_mostly();
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+  // create csr
+  start = std::chrono::system_clock::now();
+  auto upperTriangular = [](pangolin::EdgeTy<UT> e) {
+    return true; //e.first < e.second;
+  };
+  auto csr = pangolin::COO<UT>::from_edges(edges.begin(), edges.end(),
+                                                  upperTriangular);
+  LOG(debug, "nnz = {}", csr.nnz());
+  elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
+  LOG(info, "create CSR time {}s", elapsed);
+
+  // read-mostly
+  nvtxRangePush("read-mostly");
+  start = std::chrono::system_clock::now();
+  if (readMostly) {
+    for (const auto &gpu : gpus) {
+      csr.read_mostly();
+      CUDA_RUNTIME(cudaSetDevice(gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
-    elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
-    nvtxRangePop();
-    LOG(info, "read-mostly CSR time {}s", elapsed);
+  }
+  elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
+  nvtxRangePop();
+  LOG(info, "read-mostly CSR time {}s", elapsed);
 
-    // accessed-by
-    nvtxRangePush("accessed-by");
-    start = std::chrono::system_clock::now();
-    if (accessedBy) {
-      for (const auto &gpu : gpus) {
-        csr.accessed_by(gpu);
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+  // accessed-by
+  nvtxRangePush("accessed-by");
+  start = std::chrono::system_clock::now();
+  if (accessedBy) {
+    for (const auto &gpu : gpus) {
+      csr.accessed_by(gpu);
+      CUDA_RUNTIME(cudaSetDevice(gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
-    elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
-    nvtxRangePop();
-    LOG(info, "accessed-by CSR time {}s", elapsed);
+  }
+  elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
+  nvtxRangePop();
+  LOG(info, "accessed-by CSR time {}s", elapsed);
 
-    // prefetch
-    nvtxRangePush("prefetch");
-    start = std::chrono::system_clock::now();
-    if (prefetchAsync) {
-      for (const auto &gpu : gpus) {
-        csr.prefetch_async(gpu);
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+  // prefetch
+  nvtxRangePush("prefetch");
+  start = std::chrono::system_clock::now();
+  if (prefetchAsync) {
+    for (const auto &gpu : gpus) {
+      csr.prefetch_async(gpu);
+      CUDA_RUNTIME(cudaSetDevice(gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
-    elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
-    nvtxRangePop();
-    LOG(info, "prefetch CSR time {}s", elapsed);
+  }
+  elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
+  nvtxRangePop();
+  LOG(info, "prefetch CSR time {}s", elapsed);
 
-    omp_set_num_threads(152);
-    // count triangles
-    nvtxRangePush("count");
-    //start = std::chrono::system_clock::now();
+  // count triangles
+  nvtxRangePush("count");
+  //start = std::chrono::system_clock::now();
 
-    //Find Kmax
-    //This is part of file reading
-    std::map<UT, int> degree;
-    for (int i = 0; i < csr.num_rows(); i++)
-    {
-      int start = csr.rowPtr_[i];
-      int end = csr.rowPtr_[i + 1];
-  
-      if (degree.count(end - start) == 0)
-        degree[end - start] = 0;
-  
-      degree[end - start]++;
-    }
+  //Find Kmax
+  //This is part of file reading
+  std::map<UT, int> degree;
+  for (int i = 0; i < csr.num_rows(); i++)
+  {
+    int start = csr.rowPtr_[i];
+    int end = csr.rowPtr_[i + 1];
 
+    if (degree.count(end - start) == 0)
+      degree[end - start] = 0;
+
+    degree[end - start]++;
+  }
+
+  {
     start = std::chrono::system_clock::now();
-
+    int gpusPerRank = gpus.size();
+    int totalGPUs = worldSize * gpusPerRank;
+    int *rankKBounds = (int *)malloc(3*worldSize*sizeof(int));
 
     ///For stream compaction //////
     int numEdges = csr.nnz();
-    const size_t edgesPerGPU = (csr.nnz() + gpus.size() - 1) / gpus.size();
+    const size_t edgesPerGPU = (csr.nnz() + gpusPerRank - 1) / gpusPerRank;
     pangolin::Vector<UT> uSrcKp(numEdges);
     pangolin::Vector<UT> uReversed(numEdges);
     
+
+    char *keepChar = (char*) malloc(numEdges*sizeof(char));
+
     // create async counters
     std::vector<pangolin::MultiGPU_Ktruss_Binary> trussCounters;
     for (int dev : gpus) {
@@ -235,42 +258,65 @@ int main(int argc, char **argv) {
     }
     uSrcKp.read_mostly();
     uReversed.read_mostly();
-   
+    
     int kmin = 3;
     int kmax = getMaxK(degree);
-    //Attempt different k to bound kmin and kmax
-    printf("# GPUs=%d\n", gpus.size());
 
+
+    if(rank==0)
+    {
+      printf("# GPUs=%d\n", totalGPUs);
+      printf("Kmin=%d, kmax=%d\n", kmin, kmax);
+    } 
     int newKmin = kmin, newKmax=kmax;
-    int factor = 1;
+    float factor = 0.5;
     while(kmax-kmin > 1)
     {
-      //printf("New Kmin = %d, New Kmax=%d\n", kmin, kmax);
 
-      int reqGPUs = (kmax - kmin) > gpus.size()? gpus.size(): (kmax-kmin-1);
+      int totalReqGPUs = (kmax - kmin) > totalGPUs? totalGPUs:(kmax-kmin-1);
+      int reqGPUs = 0;
+      int cond = totalReqGPUs - (rank)*gpusPerRank;
+      if(cond > gpusPerRank)
+        reqGPUs = gpusPerRank;
+      else if(cond>0)
+          reqGPUs = cond;
 
-      int step = (kmax/factor-kmin)/(reqGPUs + 1);
-      factor=1;
+      if(reqGPUs == 0)
+      {
+        printf("Rank=%d is not needed any more\n", rank);
+        kmax = kmin;
+        newKmax = kmin;
+      }
+
+      int step = (kmax*factor-kmin)/(totalReqGPUs + 1);
+      factor = 1.0;
       size_t edgeStart = 0;
       int gCount = 1;
 
-     
+      printf("Rank = %d, New Kmin = %d, New Kmax=%d, total req gpus=%d, step=%d, local gpus=%d, cond=%d\n", rank, kmin, kmax, totalReqGPUs,step, reqGPUs, cond);
+      
       constexpr int dimBlock = 32; //For edges and nodes
       int dimGridEdges = (numEdges + dimBlock - 1) / dimBlock;
       UT numDeleted = 0;
       bool firstTry = true;
       
       for (auto &counter : trussCounters) 
-       {
-         counter.setDevice();
-         counter.hnumaffected[0] = 1;
-         CUDA_RUNTIME(cudaMemsetAsync(counter.gnumaffected,0,sizeof(UT),counter.stream()));
-         counter.selectedOut[0] = numEdges;
-       }
+      {
+        counter.setDevice();
+        counter.hnumaffected[0] = 1;
+        CUDA_RUNTIME(cudaMemsetAsync(counter.gnumaffected,0,sizeof(UT),counter.stream()));
+        counter.selectedOut[0] = numEdges;
+      }
 
       bool assumpAffected = true;
       dimGridEdges =  (trussCounters[0].selectedOut[0] + dimBlock - 1) / dimBlock;
       
+      for(int i=0; i<reqGPUs; i++)
+      {
+        int pk = kmin + (rank*gpusPerRank+i+1)*step;
+        printf("Rank=%d, GPU=%d, k=%d\n", rank, i, pk);
+      }
+
       while(assumpAffected)
       {
         assumpAffected = false;
@@ -282,8 +328,8 @@ int main(int argc, char **argv) {
           {
             //printf("GPU=%d, k=%d\n", counter.device(), kmin+(i+1)*step);
             core_binary_indirect<dimBlock><<<dimGridEdges,dimBlock,0,counter.stream()>>>(counter.gDstKP, counter.gnumdeleted, 
-              counter.gnumaffected, kmin + (i+1)*step, 0, counter.selectedOut[0],
-              csr.view(), counter.gKeep, counter.gAffected, uReversed.data()/*counter.gReveresed*/, firstTry, 2);
+              counter.gnumaffected, kmin + (rank*gpusPerRank+i+1)*step, 0, counter.selectedOut[0],
+              csr.view(), counter.gKeep, counter.gAffected, uReversed.data(), firstTry, 2);
 
             //Copy to host
             CUDA_RUNTIME(cudaMemcpyAsync(counter.hnumaffected, counter.gnumaffected, sizeof(UT), cudaMemcpyDeviceToHost, counter.stream()));
@@ -306,8 +352,9 @@ int main(int argc, char **argv) {
         }
         firstTry = false;
       }
-     
-      gCount = 1;
+      
+      int ks = -1;
+      int ke = -1;
       float prevPerc = 0;
       int fallBackGPU = -1;
       for (int i=0; i<reqGPUs;i++) 
@@ -318,19 +365,82 @@ int main(int argc, char **argv) {
         float perc = counter.perc_del_k();
         if(perc<1.0)
         {
-            newKmin = kmin + gCount*step;
-            fallBackGPU = gCount-1;
+            newKmin = ks = kmin + (rank*gpusPerRank+ i + 1)*step;
+            fallBackGPU = i;
         }
-        
         if(perc==1.0f && prevPerc != 1.0)
         {
-          newKmax = kmin + gCount*step;
+          newKmax = ke = kmin + (rank*gpusPerRank+ i + 1)*step;
+        }
+        prevPerc = perc;
+      }
+      //Time to all gather this
+      const int dataBase = 3;
+      int a[dataBase];
+      a[0] = ks;
+      a[1] = ke;
+      a[2] = fallBackGPU;
+      MPI_Allgather(a, dataBase, MPI_INT, rankKBounds,dataBase, MPI_INT, MPI_COMM_WORLD);
+      MPI_Barrier( MPI_COMM_WORLD);
+
+      bool foundKmax=false;
+      int root = -1;
+      for(int i= 0; i< worldSize; i++)
+      {
+        if(rankKBounds[dataBase*i] > -1)
+        {
+            newKmin=rankKBounds[dataBase*i];
+            root = i;
+            fallBackGPU = rankKBounds[dataBase*i + 2];
+        }
+        
+        if(!foundKmax && rankKBounds[dataBase*i + 1]>-1)
+        {
+          newKmax = rankKBounds[dataBase*i + 1];
+          foundKmax = true;
         }
 
-        prevPerc = perc;
-        gCount++;
+        //printf("From rank=%d, rank=%d [%d,%d] fallback=%d\t", rank, i, rankKBounds[dataBase*i], rankKBounds[dataBase*i+1], fallBackGPU);
+      }
+      //printf("\n");
+
+     if(rank==root)
+      {
+        int sum=0;
+        for (int i=0; i< numEdges; i++)
+        {
+            //sum += trussCounters[fallBackGPU].gKeep[i];
+            keepChar[i] = trussCounters[fallBackGPU].gKeep[i];
+        }
+        //printf("ROOT=%d, remaining edges = %d\n", rank, sum);
       }
 
+      //Now broadcast delete array to others
+      if(root > -1)
+      {
+        //printf("rank = %d, Let us broadcast: fallBackGPU=%d, root=%d\n", rank, fallBackGPU, root);
+        //broadcast :D
+        int ierr = MPI_Bcast(
+          //trussCounters[fallBackGPU].gKeep,
+          keepChar,
+          numEdges,
+          MPI_CHAR,
+          root,
+          MPI_COMM_WORLD);
+      }
+
+      if(fallBackGPU > -1)
+      {
+        UT sum=0;
+        for (int i=0; i< numEdges; i++)
+        {
+            trussCounters[fallBackGPU].gKeep[i] = keepChar[i];
+            //sum += trussCounters[fallBackGPU].gKeep[i];
+        }
+        //printf("rank=%d, remaining edges = %u\n", rank, sum);
+      } 
+
+      //MPI_Barrier( MPI_COMM_WORLD);
 
       for (int i=0; i<reqGPUs;i++) 
       { 
@@ -340,7 +450,7 @@ int main(int argc, char **argv) {
         {
           counter.rewind_async(numEdges);
         }
-        else if(fallBackGPU != counter.device())
+        else if(fallBackGPU != i)
         {
           counter.store_async(numEdges, trussCounters[fallBackGPU].gKeep);
         }
@@ -356,30 +466,37 @@ int main(int argc, char **argv) {
         auto& counter = trussCounters[i];
         counter.setDevice();
         counter.compact(numEdges, uSrcKp.data());
+        counter.sync();
+        //printf("rank = %d, gpu=%d, remaining edges=%d\n", rank, i, counter.selectedOut[0]);
       }
+
       kmin = newKmin;
       kmax = newKmax;
-  }
+    } 
 
-  //printf("New Kmin = %d, New Kmax=%d\n", newKmin, newKmax);
-  for (auto &counter : trussCounters)
-    counter.free();
+    //printf("New Kmin = %d, New Kmax=%d\n", newKmin, newKmax);
+    for (auto &counter : trussCounters)
+      counter.free();
 
 
     elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
     nvtxRangePop();
     LOG(info, "count time {}s", elapsed);
-    LOG(info, "MOHA {} ktruss ({} teps)", kmin, csr.nnz() / elapsed);
+    LOG(info, "Rank={} MOHA {} ktruss ({} teps)", rank, kmin, csr.nnz() / elapsed);
     times.push_back(elapsed);
     //tris = total;
     nnz = csr.nnz();
+
+    //std::cout << path << ",\t" << nnz << ",\t" << tris;
+    for (const auto &t : times) {
+      std::cout << ",\t" << t;
+    }
+    std::cout << std::endl;
+    free(rankKBounds);
   }
 
-  //std::cout << path << ",\t" << nnz << ",\t" << tris;
-  for (const auto &t : times) {
-    std::cout << ",\t" << t;
-  }
-  std::cout << std::endl;
+ 
+  MPI_Finalize();
 
   return 0;
 }
--- src/mpi_test.cu
+++ src/mpi_test.cu
@@ -5,6 +5,9 @@
 
 int main(int argc, char **argv) {
 
+
+  int kk = 10;
+
   // Initialize the MPI environment
   MPI_Init(&argc, &argv);
 
@@ -19,16 +22,42 @@ int main(int argc, char **argv) {
     printf("detected world size of %d\n", worldSize);
   }
 
+  printf("KK=%d", kk);
+
   // create an int
-  pangolin::Buffer<int> v(1);
+  //pangolin::Buffer<int> v(1);
 
   // use the GPU to set it to 1
-  pangolin::device_fill(v.data(), v.size(), 1);
-  CUDA_RUNTIME(cudaDeviceSynchronize());
+  //pangolin::device_fill(v.data(), v.size(), 1);
+  //CUDA_RUNTIME(cudaDeviceSynchronize());
+
+  printf("Rank=%d\n", rank);
+  int nDevices;
+  cudaGetDeviceCount(&nDevices);
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Rank=%d, Device Number: %d\n", rank, i);
+    /*printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (KHz): %d\n",
+           prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);*/
+
+    printf("  Bus ID: %d\n", prop.pciBusID);
+    printf("  PCI ID: %d\n", prop.pciDeviceID);
+  }
+
+
+
+  int v[1];
+  v[0] = 10;
 
   // use MPI to do a reduction
   int reduction = -1;
-  MPI_Reduce(v.data(), &reduction, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(v, &reduction, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
   // print on the rank 0 node
   if (0 == rank) {
