f6e78fcc65648a368fb9320a20988db7b26d4713
Carl Pearson <pearson@illinois.edu>
2019-07-11 13:26:19 -0500

    Add multi-gpu linear-io-queue script

---
+++ scripts/run-linear-io-queue-g01.sh
@@ -0,0 +1,19 @@
+#! env bash
+
+set -eou pipefail
+
+GRAPH_DIR=$1
+EXPERIMENT_DIR=`date +%Y%m%d-%H%M%S`_linear-io-queue-g01_`hostname`
+
+echo $EXPERIMENT_DIR
+mkdir -p $EXPERIMENT_DIR
+
+src/pangolin-version >> $EXPERIMENT_DIR/version.txt
+
+FLAGS="--bs 512 -g 0 -g 1 --prefetch-async --debug -n 5"
+
+src/benchmark-linear-io-queue $FLAGS --header >> $EXPERIMENT_DIR/run.csv 2>>$EXPERIMENT_DIR/run.log
+
+for g in $GRAPH_DIR/*.bel; do
+    numactl -p 0 src/benchmark-linear-io-queue $FLAGS $g >> $EXPERIMENT_DIR/run.csv 2>>$EXPERIMENT_DIR/run.log
+done
