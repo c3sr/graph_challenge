cfb08b853eed2fa806c5f27b6d7644f0cb14e309
Carl Pearson <carl.w.pearson@gmail.com>
2019-11-27 12:48:24 -0600

    work on bisson/fatica counter

--- src/benchmark_bissonfatica18.cu
+++ src/benchmark_bissonfatica18.cu
@@ -11,7 +11,7 @@ Count triangles using the per-edge binary search
 #include <clara/clara.hpp>
 #include <fmt/format.h>
 
-#include "pangolin/algorithm/tc_vertex_blocks_dyn.cuh"
+#include "pangolin/algorithm/tc_bissonfatica18.cuh"
 #include "pangolin/configure.hpp"
 #include "pangolin/file/edge_list_file.hpp"
 #include "pangolin/init.hpp"
@@ -20,7 +20,7 @@ Count triangles using the per-edge binary search
 struct RunOptions {
   std::string path; //!< path for graph
   std::string sep;  //!< seperator for output
-  std::vector<int> gpus;
+  int gpu;
   int dimBlock;
   int iters;
 
@@ -32,12 +32,6 @@ struct RunOptions {
 template <typename Index> int run(RunOptions &opts) {
 
   typedef typename pangolin::EdgeTy<Index> Edge;
-
-  auto gpus = opts.gpus;
-  if (gpus.empty()) {
-    LOG(warn, "no GPUs provided on command line, using GPU 0");
-    gpus.push_back(0);
-  }
 
   // read data
   auto start = std::chrono::system_clock::now();
@@ -75,9 +69,8 @@ template <typename Index> int run(RunOptions &opts) {
     auto iterStart = std::chrono::system_clock::now();
     auto competitionStart = std::chrono::system_clock::now();
     // create csr
-    auto upperTriangularFilter = [](Edge e) { return e.first < e.second; };
     auto lowerTriangularFilter = [](Edge e) { return e.first > e.second; };
-    auto csr = pangolin::CSR<uint64_t>::from_edges(edges.begin(), edges.end(), upperTriangularFilter);
+    auto csr = pangolin::CSR<Index>::from_edges(edges.begin(), edges.end(), lowerTriangularFilter);
     LOG(debug, "CSR nnz = {} rows = {}", csr.nnz(), csr.num_rows());
     elapsed = (std::chrono::system_clock::now() - iterStart).count() / 1e9;
     LOG(info, "create CSR time {}s", elapsed);
@@ -88,10 +81,8 @@ template <typename Index> int run(RunOptions &opts) {
     start = std::chrono::system_clock::now();
     if (opts.readMostly) {
       csr.read_mostly();
-      for (const auto &gpu : gpus) {
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+      CUDA_RUNTIME(cudaSetDevice(opts.gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
     elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
     nvtxRangePop();
@@ -101,11 +92,9 @@ template <typename Index> int run(RunOptions &opts) {
     // accessed-by
     start = std::chrono::system_clock::now();
     if (opts.accessedBy) {
-      for (const auto &gpu : gpus) {
-        csr.accessed_by(gpu);
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+      csr.accessed_by(opts.gpu);
+      CUDA_RUNTIME(cudaSetDevice(opts.gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
     elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
     LOG(info, "accessed-by CSR time {}s", elapsed);
@@ -114,11 +103,9 @@ template <typename Index> int run(RunOptions &opts) {
     // prefetch
     start = std::chrono::system_clock::now();
     if (opts.prefetchAsync) {
-      for (const auto &gpu : gpus) {
-        csr.prefetch_async(gpu);
-        CUDA_RUNTIME(cudaSetDevice(gpu));
-        CUDA_RUNTIME(cudaDeviceSynchronize());
-      }
+      csr.prefetch_async(opts.gpu);
+      CUDA_RUNTIME(cudaSetDevice(opts.gpu));
+      CUDA_RUNTIME(cudaDeviceSynchronize());
     }
     elapsed = (std::chrono::system_clock::now() - start).count() / 1e9;
     LOG(info, "prefetch CSR time {}s", elapsed);
@@ -127,36 +114,21 @@ template <typename Index> int run(RunOptions &opts) {
     // create async counters
     const auto counterCtorStart = std::chrono::system_clock::now();
     std::vector<pangolin::BissonFaticaTC> counters;
-    for (int dev : gpus) {
-      LOG(debug, "create device {} counter", dev);
-      counters.push_back(std::move(pangolin::BissonFaticaTC(dev)));
-    }
+    LOG(debug, "create device {} counter", opts.gpu);
+    pangolin::BissonFaticaTC counter(opts.gpu);
     elapsed = (std::chrono::system_clock::now() - counterCtorStart).count() / 1e9;
     counterCtorTimes[i] = elapsed;
 
     const auto countStart = std::chrono::system_clock::now();
 
-    // determine the number of rows per gpu
-    const size_t rowsPerGPU = (csr.num_rows() + gpus.size() - 1) / gpus.size();
-    LOG(debug, "{} rows per GPU", rowsPerGPU);
-
     // launch counting operations
-    size_t rowStart = 0;
-    for (auto &counter : counters) {
-      const size_t rowStop = std::min(rowStart + rowsPerGPU, csr.nnz());
-      const size_t numRows = rowStop - rowStart;
-      LOG(debug, "start async count on GPU {} ({} rows)", counter.device(), numRows);
-      counter.count_async(csr.view(), rowStart, numRows, opts.dimBlock);
-      rowStart += rowsPerGPU;
-    }
+    counter.count_async(csr.view(), opts.dimBlock);
 
     // wait for counting operations to finish
     uint64_t total = 0;
-    for (auto &counter : counters) {
-      LOG(debug, "wait for counter on GPU {}", counter.device());
-      counter.sync();
-      total += counter.count();
-    }
+    LOG(debug, "wait for counter on GPU {}", counter.device());
+    counter.sync();
+    total += counter.count();
 
     const auto countStop = std::chrono::system_clock::now();
 
@@ -220,6 +192,7 @@ int main(int argc, char **argv) {
   opts.readMostly = false;
   opts.accessedBy = false;
   opts.prefetchAsync = false;
+  opts.gpu = 0;
 
   bool help = false;
   bool debug = false;
@@ -233,7 +206,7 @@ int main(int argc, char **argv) {
   cli = cli | clara::Opt(verbose)["--verbose"]("print verbose messages to stderr");
   cli = cli | clara::Opt(onlyPrintHeader)["--header"]("print the header for the times output and quit");
   cli = cli | clara::Opt(wide)["--wide"]("64-bit node IDs");
-  cli = cli | clara::Opt(opts.gpus, "dev ids")["-g"]("gpus to use");
+  cli = cli | clara::Opt(opts.gpu, "device id")["-g"]("gpu to use");
   cli = cli | clara::Opt(opts.dimBlock, "block-dim")["--bs"]("Number of threads in a block");
   cli = cli | clara::Opt(opts.readMostly)["--read-mostly"]("mark data as read-mostly by all gpus before kernel");
   cli = cli | clara::Opt(opts.accessedBy)["--accessed-by"]("mark data as accessed-by all GPUs before kernel");
@@ -284,8 +257,8 @@ int main(int argc, char **argv) {
     return 0;
   }
   if (wide) {
-    return run<uint64_t>(opts);
+    return run<int64_t>(opts);
   } else {
-    return run<uint32_t>(opts);
+    return run<int32_t>(opts);
   }
 }
